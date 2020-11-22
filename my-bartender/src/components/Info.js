0296a1c352d8f870b9846ded8ff5f32beaa064d4
github username <github email address>
2020-11-22 16:29:00 -0500

    more show page work

--- my-bartender/src/components/Info.js
+++ my-bartender/src/components/Info.js
@@ -1,27 +1,66 @@
 import React, {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+
 
 function Info(props) {
 
-    // const [table, setTable] = useState([]);
+    const classes = useStyles();
 
-    // useEffect(()=> {
-    //     fetch("http://localhost:3000/foods",{
-    //       method: "get",
-    //       mode: 'cors',
-    //       headers: {'Content-Type': 'application/json'}
-    //     }).then(data => data.json())
-    //     .then(data => setFoods(data))
-    //   }, [])
+    const [table, setTable] = useState([]);
+    const [groupSize, setSize] = useState()
 
+    useEffect(()=> {
+        fetch(`http://localhost:3000/tables/${props.tableId}`,{
+          method: "get",
+          mode: 'cors',
+          headers: {'Content-Type': 'application/json'}
+        }).then(data => data.json())
+        .then(data => setTable(data))
+      },[props.tableId])
+
+    const handleChange = (event) =>{
+
+    }
+
+    const createGroup = () => {
+        fetch(`http://localhost:3000/groups/new`, {
+            method: "post",
+            mode: 'cors',
+            headers:{'Content-Type': 'application/json'}
+
+        }
+
+        )
+    }
       
-
+      
 
     return (
         <div>
-            <h3>Table {props.tableId}</h3>
-
-            <p>If table is empty add group functionality goes here</p>
-            <p>Else let the group order food from here</p>
+            <h3>Table {table.id}</h3>
+    {table.group? <p>Table {table.id} has a group with {table.group.num_people} at it</p>:
+    <>
+    <p>This table is Empty</p>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField 
+      id="groupSize" 
+      label="number of people" />
+    </form>
+    <Button variant="contained" onClick= {() => console.log("hi")}>Add a new Group</Button>
+    </>
+    }
 
         </div>
     );
