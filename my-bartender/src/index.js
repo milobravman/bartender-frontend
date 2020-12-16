3b5c92f7ed8539ec7f0ff49a6a5faf6d1be18b11
Milo Bravman <Milobravman20@gmail.com>
2020-12-16 11:27:07 -0500

    comment on index

--- my-bartender/src/index.js
+++ my-bartender/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// setting up react-routes
 import { BrowserRouter } from 'react-router-dom';
 
 
