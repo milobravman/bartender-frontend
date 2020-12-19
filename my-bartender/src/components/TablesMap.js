de456d5a32642e25a2d1a797c593b7c2a36e13d9
Milo Bravman <Milobravman20@gmail.com>
2020-12-19 15:20:51 -0500

    last comment of the day

--- my-bartender/src/components/TablesMap.js
+++ my-bartender/src/components/TablesMap.js
@@ -3,12 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import Button from '@material-ui/core/Button';
-// import { Link } from 'react-router-dom' 
 
+
+// material-ui CSS
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -20,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
   gridList: {
     width: 500,
     height: 450,
+    marginTop: "1%"
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
@@ -32,7 +33,8 @@ function TablesMap(props) {
 
     const [tables, setTables] = useState([]);
 
-
+    //fetching Table data which indludes groups 
+    // may need to have a nested includes to add the no_orders, has_ordered, and been_delivered feature
     useEffect(()=> {
       fetch("http://localhost:3000/tables",{
         method: "get",
@@ -42,48 +44,45 @@ function TablesMap(props) {
       .then(data => setTables(data))
     }, [])
 
-    return (
-        // <ul>
-        //     {tables.map(table => {
-        //         return <li key = {table.id}>{table.position}</li>
-        //     })}
-        // </ul>
-      <div className={classes.root}>
-        <GridList cellHeight={180} className={classes.gridList}>
-          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div">
-              Table Overview
-            </ListSubheader>
-          </GridListTile>
 
-            {tables.map((table) => (
-              <GridListTile 
-                key={table.id} 
-                onClick = {() => props.showTable(table.id)}
-                style = {{border: "solid #000",
-                  borderWidth: "1px"}}
-              >
-                  {table.group?
-                    <h3>
-                      {`Table occupied with ${table.group.num_people} people`}
-                    </h3>:
-                    <h3>
-                      Empty
-                    </h3>
-                  }
-                    <GridListTileBar
-                      
-                title={
-                <Button style = {{ backgroundColor: "#add8e6"}}>{`Table: ${table.position}`}</Button>}
-                      actionIcon={
-                      <IconButton 
-                        aria-label={`info about ${table.position}`} 
-                        className={classes.icon}>
-                        <InfoIcon />
-                      </IconButton>}/>
-            </GridListTile>))}
-        </GridList>
-      </div>
+    //JSX with very confusing CSS the Gridlist ul has a margin of -2. Why I do not know
+    return (
+      <>
+      <h3>Table Overview</h3>
+        <div className={classes.root}>
+          <GridList cellHeight={180} className={classes.gridList}>
+            <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+            </GridListTile>
+
+              {tables.map((table) => (
+                <GridListTile 
+                  key={table.id} 
+                  onClick = {() => props.showTable(table.id)}
+                  style = {{border: "solid #000",
+                    borderWidth: "1px"}}
+                >
+                    {table.group?
+                      <h3>
+                        {`Table occupied with ${table.group.num_people} people`}
+                      </h3>:
+                      <h3>
+                        Empty
+                      </h3>
+                    }
+                      <GridListTileBar
+                        
+                  title={
+                  <Button style = {{ backgroundColor: "#add8e6"}}>{`Table: ${table.position}`}</Button>}
+                        actionIcon={
+                        <IconButton 
+                          aria-label={`info about ${table.position}`} 
+                          className={classes.icon}>
+                          <InfoIcon />
+                        </IconButton>}/>
+              </GridListTile>))}
+          </GridList>
+        </div>
+      </>
     );
 }
 
